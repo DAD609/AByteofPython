2fbe3b04d61eebe0bac08c37b53ae50c11877b11
ADam <67281187+DAD609@users.noreply.github.com>
2023-07-08 18:03:26 +0300

    Update raising.py

--- Code/raising.py
+++ Code/raising.py
@@ -14,4 +14,6 @@ print('Ну зачем вы сделали мне EOF?')
 except ShortInputException as ex:
 print('ShortInputException: Длина введённой строки -- {0}; \
 ожидалось, как минимум, {1}'.format(ex.length, ex.atleast))
-(продолжение на следующей с
+
+else:
+print('Не было исключений.')
